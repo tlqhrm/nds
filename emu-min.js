d1deec11d87a428232347816f0cb39af3c0e87e5
tlqhrm <85216782+tlqhrm@users.noreply.github.com>
2022-04-14 19:02:24 +0900

    Add files via upload

--- emu-min.js
+++ emu-min.js
@@ -15880,7 +15880,10 @@ var EJS = function(e) {
                     5: "DOWN",
                     6: "LEFT",
                     7: "RIGHT",
-                    8: "A"
+                    8: "A",
+                    24: e.localization("QUICK SAVE STATE"),
+                    25: e.localization("QUICK LOAD STATE"),
+                    26: e.localization("CHANGE STATE SLOT")
                 };
                 else if ("snes" === e.system) o = {
                     0: "B",
@@ -15894,7 +15897,10 @@ var EJS = function(e) {
                     8: "A",
                     9: "X",
                     10: "L",
-                    11: "R"
+                    11: "R",
+                    24: e.localization("QUICK SAVE STATE"),
+                    25: e.localization("QUICK LOAD STATE"),
+                    26: e.localization("CHANGE STATE SLOT")
                 };
                 else if ("n64" === e.system) o = {
                     0: "B",
@@ -15914,7 +15920,10 @@ var EJS = function(e) {
                     23: "R STICK UP",
                     22: "R STICK DOWN",
                     21: "R STICK LEFT",
-                    20: "R STICK RIGHT"
+                    20: "R STICK RIGHT",
+                    24: e.localization("QUICK SAVE STATE"),
+                    25: e.localization("QUICK LOAD STATE"),
+                    26: e.localization("CHANGE STATE SLOT")
                 };
                 else if ("nds" === e.system) o = {
                     0: "B",
@@ -15929,7 +15938,10 @@ var EJS = function(e) {
                     9: "X",
                     10: "L",
                     11: "R",
-                    14: "Microphone"
+                    14: "Microphone",
+                    24: e.localization("QUICK SAVE STATE"),
+                    25: e.localization("QUICK LOAD STATE"),
+                    26: e.localization("CHANGE STATE SLOT")
                 };
                 else o = {
                     0: "B",
@@ -15955,7 +15967,10 @@ var EJS = function(e) {
                     23: "R STICK UP",
                     22: "R STICK DOWN",
                     21: "R STICK LEFT",
-                    20: "R STICK RIGHT"
+                    20: "R STICK RIGHT",
+                    24: e.localization("QUICK SAVE STATE"),
+                    25: e.localization("QUICK LOAD STATE"),
+                    26: e.localization("CHANGE STATE SLOT")
                 };
                 ["arcade", "mame"].includes(e.system) && (o[2] = e.localization("INSERT COIN")), !1 === e.statesSupported && (delete o[24], delete o[25], delete o[26]);
                 for (var r = [2, 3, 4, 5, 6, 7, 8, 0, 9, 1, 10, 11, 12, 13, 14, 15, 19, 18, 17, 16, 23, 22, 21, 20, 24, 25, 26], a = function(a) {
@@ -17155,48 +17170,48 @@ var EJS = function(e) {
                         at.connection.shareFile(d), at.inputsData = {}, at.wait = !0, at.systemPause(1), Ke.Module.pauseMainLoop(), at.disableControl(!0)
                     }
                     e.elements.container.focus()
-                // }, Ke.Module._set_cheat && (at.setCheat = Ke.Module.cwrap("set_cheat", "number", ["number", "number", "string"])), Ke.Module._reset_cheat && (at.resetCheat = Ke.Module._reset_cheat), at.quickSaveState = function() {
-                //     if (e.started && !at.connected) {
-                //         if (!1 === e.statesSupported) return e.elements.widgets.stateInfoDiv.innerHTML = "CANNOT CURRENTLY SAVE STATE", clearTimeout(at.saveMsgTransitions), void(at.saveMsgTransitions = setTimeout((function() {
-                //             e.elements.widgets.stateInfoDiv.innerHTML = ""
-                //         }), 1500));
-                //         if (ct && "function" == typeof ct.getCoreOptionsValues)(t = ct.getCoreOptionsValues()["save-state-slot"]) || (t = 1);
-                //         else var t = 1;
-                //         var n = t + "-quick.state";
-                //         try {
-                //             Ke.FS.unlink(n)
-                //         } catch (e) {}
-                //         try {
-                //             var o = at.saveState();
-                //             Ke._FS.createDataFile("/", n, o, !0, !0), e.elements.widgets.stateInfoDiv.innerHTML = "STATE SAVED TO SLOT " + t, clearTimeout(at.saveMsgTransitions), at.saveMsgTransitions = setTimeout((function() {
-                //                 e.elements.widgets.stateInfoDiv.innerHTML = ""
-                //             }), 1500)
-                //         } catch (n) {
-                //             e.elements.widgets.stateInfoDiv.innerHTML = "FAILED TO SAVE STATE TO SLOT " + t, clearTimeout(at.saveMsgTransitions), at.saveMsgTransitions = setTimeout((function() {
-                //                 e.elements.widgets.stateInfoDiv.innerHTML = ""
-                //             }), 1500)
-                //         }
-                //     }
-                // }, at.saveMsgTransitions = null, at.quickLoadState = function() {
-                //     if (e.started && !at.connected) {
-                //         if (!1 === e.statesSupported) return e.elements.widgets.stateInfoDiv.innerHTML = "CANNOT CURRENTLY LOAD STATE", clearTimeout(at.saveMsgTransitions), void(at.saveMsgTransitions = setTimeout((function() {
-                //             e.elements.widgets.stateInfoDiv.innerHTML = ""
-                //         }), 1500));
-                //         if (ct && "function" == typeof ct.getCoreOptionsValues)(n = ct.getCoreOptionsValues()["save-state-slot"]) || (n = 1);
-                //         else var n = 1;
-                //         var o = n + "-quick.state",
-                //             r = !0;
-                //         try {
-                //             a(o, 0), "arcade" === t && setTimeout((function() {
-                //                 at.getStateInfo(), a(o, 0)
-                //             }), 10)
-                //         } catch (e) {
-                //             r = !1
-                //         }
-                //         e.elements.widgets.stateInfoDiv.innerHTML = r ? "LOADED STATE FROM SLOT " + n : "FAILED TO LOAD STATE FROM SLOT " + n, clearTimeout(at.saveMsgTransitions), at.saveMsgTransitions = setTimeout((function() {
-                //             e.elements.widgets.stateInfoDiv.innerHTML = ""
-                //         }), 1500)
-                //     }
+                }, Ke.Module._set_cheat && (at.setCheat = Ke.Module.cwrap("set_cheat", "number", ["number", "number", "string"])), Ke.Module._reset_cheat && (at.resetCheat = Ke.Module._reset_cheat), at.quickSaveState = function() {
+                    if (e.started && !at.connected) {
+                        if (!1 === e.statesSupported) return e.elements.widgets.stateInfoDiv.innerHTML = "CANNOT CURRENTLY SAVE STATE", clearTimeout(at.saveMsgTransitions), void(at.saveMsgTransitions = setTimeout((function() {
+                            e.elements.widgets.stateInfoDiv.innerHTML = ""
+                        }), 1500));
+                        if (ct && "function" == typeof ct.getCoreOptionsValues)(t = ct.getCoreOptionsValues()["save-state-slot"]) || (t = 1);
+                        else var t = 1;
+                        var n = t + "-quick.state";
+                        try {
+                            Ke.FS.unlink(n)
+                        } catch (e) {}
+                        try {
+                            var o = at.saveState();
+                            Ke._FS.createDataFile("/", n, o, !0, !0), e.elements.widgets.stateInfoDiv.innerHTML = "STATE SAVED TO SLOT " + t, clearTimeout(at.saveMsgTransitions), at.saveMsgTransitions = setTimeout((function() {
+                                e.elements.widgets.stateInfoDiv.innerHTML = ""
+                            }), 1500)
+                        } catch (n) {
+                            e.elements.widgets.stateInfoDiv.innerHTML = "FAILED TO SAVE STATE TO SLOT " + t, clearTimeout(at.saveMsgTransitions), at.saveMsgTransitions = setTimeout((function() {
+                                e.elements.widgets.stateInfoDiv.innerHTML = ""
+                            }), 1500)
+                        }
+                    }
+                }, at.saveMsgTransitions = null, at.quickLoadState = function() {
+                    if (e.started && !at.connected) {
+                        if (!1 === e.statesSupported) return e.elements.widgets.stateInfoDiv.innerHTML = "CANNOT CURRENTLY LOAD STATE", clearTimeout(at.saveMsgTransitions), void(at.saveMsgTransitions = setTimeout((function() {
+                            e.elements.widgets.stateInfoDiv.innerHTML = ""
+                        }), 1500));
+                        if (ct && "function" == typeof ct.getCoreOptionsValues)(n = ct.getCoreOptionsValues()["save-state-slot"]) || (n = 1);
+                        else var n = 1;
+                        var o = n + "-quick.state",
+                            r = !0;
+                        try {
+                            a(o, 0), "arcade" === t && setTimeout((function() {
+                                at.getStateInfo(), a(o, 0)
+                            }), 10)
+                        } catch (e) {
+                            r = !1
+                        }
+                        e.elements.widgets.stateInfoDiv.innerHTML = r ? "LOADED STATE FROM SLOT " + n : "FAILED TO LOAD STATE FROM SLOT " + n, clearTimeout(at.saveMsgTransitions), at.saveMsgTransitions = setTimeout((function() {
+                            e.elements.widgets.stateInfoDiv.innerHTML = ""
+                        }), 1500)
+                    }
                 }, at.changeStateSlot = function(t) {
                     var n;
                     if (ct && "function" == typeof ct.getCoreOptionsValues)(o = ct.getCoreOptionsValues()["save-state-slot"]) || (o = 1);
@@ -17765,7 +17780,20 @@ var EJS = function(e) {
                     default:
                         e = {}, console.warn("Unsupported emulator")
                 }
-                
+                return 2 === this.coreVer && (e = {}), !0 === this.statesSupported && it.getGameCoreOptions && it.getGameCoreOptions().split("\n").forEach((function(t, n) {
+                    var o = t.split("; "),
+                        r = o[0];
+                    if (0 === r.indexOf("fba-dipswitch-")) {
+                        var a = o[1].split("|"),
+                            i = r.replace(/_/g, " ").replace(/.+\-(.+)/, "$1");
+                        e[r] = {
+                            label: i,
+                            options: a.slice(1, -1),
+                            default: a[0].replace("(Default) ", ""),
+                            netplay: !0
+                        }
+                    }
+                })), e
             },
             supportNetPlay: function() {
                 if (this.lightgun || this.mouse) return !1;
